618ca5700804e257dd4b5c03a188cedde4eb0074
dominik.leitner.fab@gmail.com <dominik.leitner.fab@gmail.com>
2024-10-30 20:27:01 +0000

    Edited get_temperature_data

--- server_code/get_temperature_data.py
+++ server_code/get_temperature_data.py
@@ -1,12 +1,13 @@
 import anvil.server
-
+from anvil.server import callable
+from .get_mysql import get_db_connection
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
 # To allow anvil.server.call() to call functions here, we mark
 @anvil.server.callable
 def get_temperature_data():
-    conn = get_mysql()
+    conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT TIMESTAMP,sensorValue1 FROM tooltronic.measurement ORDER BY timestamp DESC LIMIT 100")
     data = cursor.fetchall()

be6e5d5117b5e1a4ccb822fc1b1dd1e6549322ee
dominik.leitner.fab@gmail.com <dominik.leitner.fab@gmail.com>
2024-10-30 21:29:26 +0000

    Edited get_mysql; Deleted button.css and ServerPackage1

--- server_code/get_mysql.py
+++ server_code/get_mysql.py
@@ -3,10 +3,15 @@ import mysql.connector
 from anvil.server import callable
 
 def get_db_connection():
+  try:
     connection = mysql.connector.connect(
         host='192.168.1.12',       # z.B. 'localhost' oder die IP-Adresse Ihres Datenbankservers
         user='root',   # Ihr Datenbankbenutzername
         password='Sensid.DB',# Ihr Datenbankpasswort
         database='tooltronic'    # Der Name Ihrer Datenbank
     )
+    
     return connection
+  except mysql.connector.Error as err:
+        print(f"Datenbankverbindungsfehler: {err}")
+        raise

b7fd8770c049376b7bd937133155003eaae0b2ea
dominik.leitner.fab@gmail.com <dominik.leitner.fab@gmail.com>
2024-10-30 20:50:22 +0000

    Edited get_temperature_data

--- server_code/get_temperature_data.py
+++ server_code/get_temperature_data.py
@@ -9,7 +9,7 @@ from .get_mysql import get_db_connection
 def get_temperature_data():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT TIMESTAMP,sensorValue1 FROM tooltronic.measurement ORDER BY timestamp asc LIMIT 100")
+    cursor.execute("SELECT DATE_FORMAT(TIMESTAMP, '%Y-%m-%d %H:%i:%S'),sensorValue1 FROM tooltronic.measurement ORDER BY timestamp desc LIMIT 100")
     data = cursor.fetchall()
     cursor.close()
     conn.close()
